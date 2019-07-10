6759ae8ee0a2cd6dec788b255ffa1a3062604237
Philippe Kotar <philippe.kotar@gmail.com>
2019-07-10 12:03:08 -0400

    Yearly Balance Sheet Complete

--- portfolio/static/portfolio/js/custom.js
+++ portfolio/static/portfolio/js/custom.js
@@ -1,3 +1,30 @@
-.jumbotron{
+$(document).ready(function() { 
+    $('#descriptionCard').hide()
+
+
+
+
+
+    // CARDS ---------------------------------------------------------------------------
+    // Description Card ----------------------------------------------------------------
+    $('#navDescriptionCard').click(function(){
+        $('#financialsCard').hide()
+        $('#descriptionCard').show()
+        $('a#navFinancialsCard').toggleClass("active")
+        $('a#navDescriptionCard').toggleClass("active")
+    });
+
+    // Financials Card ----------------------------------------------------------------
+    $('#navFinancialsCard').click(function(){
+        $('#financialsCard').show()
+        $('#descriptionCard').hide()
+        $('a#navFinancialsCard').toggleClass("active")
+        $('a#navDescriptionCard').toggleClass("active")
+    });
     
-}
+    
+
+
+
+
+ }); // document.ready()
--- research/views.py
+++ research/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from portfolio.forms import SymbolForm, AddPositionForm
 from folio_viz.helpers.url_functions import get_fundamentals, get_realTime
@@ -8,6 +8,9 @@ import requests
 import pandas as pd
 import json
 
+
+
+
 # MUST CHANGE IN PRODUCTION
 api_key = '5cf16f2040e332.31358607'
 
@@ -15,23 +18,123 @@ api_key = '5cf16f2040e332.31358607'
 def research_home(request):
     # Create forms
     form = SymbolForm(request.GET or None)
-    user_portfolios = Portfolio.objects.filter(user=request.user)
-    addPositionForm = AddPositionForm(**{'user': request.user}, initial={'shares': 100})
+    # addPositionForm = AddPositionForm(**{'user': request.user}, initial={'shares': 100, 'date': '2019-06-04', 'price': 2})
+    data = {
+        'shares': 100,
+        'price': 10,
+        'date': '2019-05-01',
+        'commission': 4.99
+    }
+
+    if request.method == 'POST':
+        # print(request.POST)
+        updated_request = request.POST.copy()
+        formSymbol = request.GET.get('symbol')
+
+        formShares = request.POST.get('shares')
+        formPrice = request.POST.get('price')
+        formTransaction = request.POST.get('transaction_type')
+ 
+        updated_request.update({'symbol': formSymbol})
+        addPositionForm = AddPositionForm(updated_request)
+        if addPositionForm.is_valid():
+            addPositionForm.save()
+            if 'BUY' in addPositionForm.cleaned_data.get('transaction_type'):
+                formTransaction = 'BOUGHT'
+            else:
+                formTransaction = 'SOLD'
+            formPortfolio = addPositionForm.cleaned_data.get('portfolio')
+            formShares = addPositionForm.cleaned_data.get('shares')
+            formPrice = addPositionForm.cleaned_data.get('price')
+            formSymbol = formSymbol.upper()
+            
+            # username = addPositionForm.cleaned_data.get('username')
+            messages.success(request, f'{formTransaction} {formShares} shares of\
+                                        {formSymbol} at ${formPrice} in portfolio {formPortfolio}.')
+            return redirect('research_home') 
+    else:
+        addPositionForm = AddPositionForm(**{'user': request.user}, initial=data)
+
+
+
+
+
     
-    # Validate if symbol form is valid
+    # Validate if SYMBOL form is valid
     if form.is_valid():
         symbol = form['symbol'].value()
         fundamentals =  get_fundamentals(symbol, api_key)
         realTime =  get_realTime(symbol, api_key)
+
+        balanceSheetData = []
+        
+        
+        for value in fundamentals['Financials']['Balance_Sheet']['yearly'].items():
+            for k, v in value[1].items():
+                if v is None:
+                    value[1][k] = '-'
+            
+                try:
+                    v = float(v)
+                    value[1][k] = int(v / 1000000)
+                except:
+                    continue                
+            balanceSheetData.append(value)    
+            
+                      
+                
+        # print(balanceSheetData)
+                
+        
+                
+                
+
+
+
+
+
         if fundamentals and realTime:  
             realTime['timestamp'] = datetime.fromtimestamp(realTime['timestamp'])
             realTime['change_p'] = float("{0:.2f}".format(realTime['change_p'] * 100))
             if realTime['change'] < 0:
                 realTime['changeDown'] = True
+         
+
+            
+                
+                # if value is None:
+                #     value = '-'
+                # print(key)
+                # balanceSheetData[key] = 'test'
+                # print(balanceSheetData[key])
+                # try:
+                #     # print(type(value['totalLiab']))
+                #     totalCurrentLiabilities = float(value['totalCurrentLiabilities'])
+                #     accountsPayable = float(value['accountsPayable'])
+                #     otherCurrentLiab = float(value['otherCurrentLiab'])
+                #     shortLongTermDebt = float(value['shortLongTermDebt'])
+                #     additionalLiabilities = totalCurrentLiabilities - (accountsPayable + otherCurrentLiab +shortLongTermDebt)
+                # except:
+                #     continue
+                # value['otherCurrentLiab'] = str(float(value['otherCurrentLiab']) + additionalLiabilities)
+
+
+            
+
+
+
+
+
+
+
+
+
+
             context = {
                 'fundamentals': fundamentals,
                 'addPositionForm': addPositionForm,
-                'realTime': realTime
+                'realTime': realTime,
+                'balanceSheetData': balanceSheetData
             }
             return render(request, 'research/research_home.html', context)   
         else:
