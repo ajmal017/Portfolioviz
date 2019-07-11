e8ce4cc6229afca0232ad07f3c1c5d46bfe3e53c
Philippe Kotar <philippe.kotar@gmail.com>
2019-07-11 13:23:30 -0400

    Fixed balance sheet and income statment table design

--- portfolio/static/portfolio/js/custom.js
+++ portfolio/static/portfolio/js/custom.js
@@ -1,10 +1,10 @@
 $(document).ready(function() { 
     $('#descriptionCard').hide()
-    $('#blanceSheetYearlyTableWrapper').hide()
+    $('#incomeStatementYearlyWrapper').hide()
 
     
    
-    // $('#').DataTable();
+    // $('#').DataTable(); ----------------------------------------------------------------------------
     $('#incomeStatementYearlyTable').DataTable({
         "paging": false,
         "scrollX": true,
@@ -12,8 +12,17 @@ $(document).ready(function() {
         "ordering": false,
         "bInfo": false,
         "bAutoWidth": true
-        
       });
+    
+    $('#balanceSheetYearlyTable').DataTable({
+        "paging": false,
+        "scrollX": true,
+        "searching": false,
+        "ordering": false,
+        "bInfo": false,
+        "bAutoWidth": true
+      });
+
 
 
     // CARDS ---------------------------------------------------------------------------
@@ -32,10 +41,39 @@ $(document).ready(function() {
         $('a#navFinancialsCard').toggleClass("active")
         $('a#navDescriptionCard').toggleClass("active")
     });
-    
+
+        // Financial Statement Buttons ----------------------------------------------------------------
+        $('#btnBalanceSheet').click(function(){
+            $('#incomeStatementYearlyWrapper').hide();
+            $('#blanceSheetYearlyWrapper').show();
+            $( '#balanceSheetYearlyTable' ).DataTable().columns.adjust();
+            $( '#btnBalanceSheet' ).removeClass( "btn-outline-info" )
+            $( '#btnBalanceSheet' ).addClass( "btn-info" );
+            $( '#btnIncomeStatement, #btnCashFlowStatement' ).removeClass( "btn-info" )
+            $( '#btnIncomeStatement, #btnCashFlowStatement' ).addClass( "btn-outline-info" );
+        });
+
+        $('#btnIncomeStatement').click(function(){
+            $('#blanceSheetYearlyWrapper').hide()
+            $('#incomeStatementYearlyWrapper').show()
+            $( '#incomeStatementYearlyTable' ).DataTable().columns.adjust();
+            $( '#btnIncomeStatement' ).removeClass( "btn-outline-info" )
+            $( '#btnIncomeStatement' ).addClass( "btn-info" );
+            $( '#btnBalanceSheet, #btnCashFlowStatement' ).removeClass( "btn-info" )
+            $( '#btnBalanceSheet, #btnCashFlowStatement' ).addClass( "btn-outline-info" );
+        });
+
+        $('#btnCashFlowStatement').click(function(){
+            $( '#btnCashFlowStatement' ).removeClass( "btn-outline-info" )
+            $( '#btnCashFlowStatement' ).addClass( "btn-info" );
+            $( '#btnIncomeStatement, #btnBalanceSheet' ).removeClass( "btn-info" )
+            $( '#btnIncomeStatement, #btnBalanceSheet' ).addClass( "btn-outline-info" );
+        });
+
     
 
-
+    
+    
 
 
  }); // document.ready()
